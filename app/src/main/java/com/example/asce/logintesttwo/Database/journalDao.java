de2b8ca86872229960e4f2f0ffffa6104ffd3bbd
Asce <samuelnyamai7387@gmail.com>
2018-06-26 11:45:18 +0300

    Second commit . Added roomdatabse

--- app/src/main/java/com/example/asce/logintesttwo/Database/journalDao.java
+++ app/src/main/java/com/example/asce/logintesttwo/Database/journalDao.java
@@ -3,7 +3,10 @@ package com.example.asce.logintesttwo.Database;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface journalDao {
@@ -16,4 +19,9 @@ public interface journalDao {
 
     @Delete
     void deletejournal (Journals journals);
+
+    @Query("SELECT * FROM journals")
+    List<Journals> alljournals ();
+
+
 }
